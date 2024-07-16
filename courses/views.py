a914e5de493707bf93cee75263bd722b9542877d
Ali <Alihunter4640@gmail.com>
2024-07-16 21:34:11 +0330

    views increment in django list view
    implemented with overriding the django listview get queryset method and atomic transactions

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView 
+from django.db import transaction
 from .models import *
 
 
@@ -7,3 +10,16 @@ from .models import *
 class VideosListView(ListView):
     model = Course
     template_name = 'courses/videos.html'
+
+    def get_queryset(self):
+        # Use a transaction to ensure atomicity
+        with transaction.atomic():
+            # Get the queryset
+            queryset = super().get_queryset()
+            
+            # Increment the views field for each object
+            for obj in queryset:
+                obj.views += 1
+                obj.save()  # Save each object with the new value
+            
+        return queryset
