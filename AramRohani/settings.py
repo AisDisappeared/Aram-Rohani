123c07f0c68e550b84de0ad911b78843f7c1c77f
Ali <Alihunter4640@gmail.com>
2024-07-16 16:47:10 +0330

    3 apps made and registered
    website,courses and accounts

--- AramRohani/settings.py
+++ AramRohani/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # our apps
+    'accounts',
+    'courses',
+    'website',
 
     # 3rd party apps 
     'tailwind',
