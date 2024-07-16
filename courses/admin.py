96373f43b98f46e37300b03cef1efd610d404c5b
Ali <Alihunter4640@gmail.com>
2024-07-16 17:42:56 +0330

    courses model and registered in admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import * 
 
-# Register your models here.
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    list_filter = ['name']
+    search_fields = ['name'] 
+    save_as = True
+    save_on_top = True 
+    search_help_text = f'search in: {". ".join(search_fields)}'
+
+
+
+class CourseAdmin(admin.ModelAdmin):
+    date_hierarchy = 'published_date'
+    list_filter = ['title','tag']
+    list_display = ['title','tag','views','published_date']
+    search_fields = ['title'] 
+    save_as = True
+    save_on_top = True 
+    search_help_text = f'search in: {". ".join(search_fields)}'
+
+
+
+
+admin.site.register(Tag,TagAdmin)
+admin.site.register(Course,CourseAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    slug = models.SlugField(blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    tag = models.ForeignKey(Tag,on_delete=models.CASCADE,blank=True)
+    views = models.BigIntegerField(default=0)
+    video = models.FileField(upload_to='videos/')
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-published_date']
+
+    def __str__(self):
+        return f"{self.title}"
