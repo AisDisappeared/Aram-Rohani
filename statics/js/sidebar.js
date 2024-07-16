4580dc91cf5b931b7449c7cac13163e0ec46549a
Ali <Alihunter4640@gmail.com>
2024-07-16 19:04:05 +0330

    sidebar's javascript code changed
    closing sidebar when we move our mouse out of the sidebar

--- statics/js/sidebar.js
+++ statics/js/sidebar.js
@@ -1,4 +1,21 @@
-    document.addEventListener('DOMContentLoaded', function() {
+    // sidebar will closed when click on out of sidebar
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     const sidebarToggle = document.querySelector('[data-drawer-toggle="sidebar-multi-level-sidebar"]');
+    //     const sidebar = document.getElementById('sidebar-multi-level-sidebar');
+
+    //     sidebarToggle.addEventListener('click', function() {
+    //         sidebar.classList.toggle('-translate-x-full');
+    //     });
+
+    //     document.addEventListener('click', function(event) {
+    //         if (!sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
+    //             sidebar.classList.add('-translate-x-full');
+    //         }
+    //     });
+    // });
+
+       // sidebar will closed when we move the mouse out of the sidebar
+        document.addEventListener('DOMContentLoaded', function() {
         const sidebarToggle = document.querySelector('[data-drawer-toggle="sidebar-multi-level-sidebar"]');
         const sidebar = document.getElementById('sidebar-multi-level-sidebar');
 
@@ -6,9 +23,7 @@
             sidebar.classList.toggle('-translate-x-full');
         });
 
-        document.addEventListener('click', function(event) {
-            if (!sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
-                sidebar.classList.add('-translate-x-full');
-            }
+        sidebar.addEventListener('mouseleave', function() {
+            sidebar.classList.add('-translate-x-full');
         });
     });
