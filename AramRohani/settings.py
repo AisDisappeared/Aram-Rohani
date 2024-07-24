55830602932bf0611214e46e6c443e637b5226ce
Ali <Alihunter4640@gmail.com>
2024-07-24 12:57:31 +0330

    smtp system for sending email

--- AramRohani/settings.py
+++ AramRohani/settings.py
@@ -210,3 +210,15 @@ AUTHENTICATION_BACKENDS = [
 # Robot text FILE
 ROBOTS_USE_HOST = True
 ROBOTS_USE_SITEMAP = True
+
+
+
+
+
+# django smtp email backend settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'officialaramrohani@gmail.com'
+EMAIL_HOST_PASSWORD = 'docggeaenxmiwrno'
