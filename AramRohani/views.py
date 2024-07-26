e39ea16ad2b44b35e05c4e7df77d6625b4df93d7
Ali <Alihunter4640@gmail.com>
2024-07-26 14:17:15 +0330

    some issues fixed

--- AramRohani/views.py
+++ AramRohani/views.py
@@ -12,6 +12,18 @@ from django.urls import reverse
 #     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+# def change_mode(request):
+#     # Toggle the dark mode preference
+#     if request.session.get('is_dark_mode', False):
+#         request.session['is_dark_mode'] = False
+#     else:
+#         request.session['is_dark_mode'] = True
+    
+#     # Redirect back to the referring page or a default page
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER', reverse('website:home')))
+
+
+
 def change_mode(request):
     # Toggle the dark mode preference
     if request.session.get('is_dark_mode', False):
@@ -20,4 +32,8 @@ def change_mode(request):
         request.session['is_dark_mode'] = True
     
     # Redirect back to the referring page or a default page
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER', reverse('website:home')))
+    referer = request.META.get('HTTP_REFERER')
+    if referer:
+        return HttpResponseRedirect(referer)
+    else:
+        return HttpResponseRedirect(reverse('website:home'))
